699283b774568025b5dd72ab236f0145f1bc7575
Brandon Westover <bwestove@ALE5EA8E.its.caregroup.org>
2025-02-15 22:38:45 -0500

    e Please enter the commit message for your changes. Lines starting

--- old/fcnGetSubjectiveDifficulty.m
+++
@@ -1,7 +0,0 @@
-function [qd1,qd2] = fcnGetSubjectiveDifficulty(T1,T2,ind);
-
-qd1 = T1(:,6:3:end); % perceived difficulty
-qd2 = T2(:,4:3:end); % perceived difficulty
-
-qd1 = table2array(qd1); 
-qd2 = table2array(qd2); 
